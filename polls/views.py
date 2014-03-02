5c510fe148f25e3b48a457628eff4356ed0d00d3
Rafael Jamur <jamur@ufpr.br>
2014-03-02 14:00:33 -0300

    Cap. 5 - Completo - Testes do Tutorial feitos
    Métodos não usados no views.py movidos para baixo

--- polls/views.py
+++ polls/views.py
@@ -16,11 +16,6 @@ class IndexView(generic.ListView):
 		pub_date__lte=timezone.now()
 	).order_by('-pub_date')[:5]
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
-
 
 class DetailView(generic.DetailView):
     model = Question
@@ -32,18 +27,10 @@ class DetailView(generic.DetailView):
         """
         return Question.objects.filter(pub_date__lte=timezone.now())
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
-
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
@@ -63,4 +50,18 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button
         return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+
+# metodos nao usados mais
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
